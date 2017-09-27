7253f61d9561fe653197e51a0555dc8c90fdd40f
Saul <digitalh2o@gmail.com>
2017-09-26 21:25:33 -0500

    Added React Router and Fallback page

---
+++ app/components/List.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+class List extends React.Component {
+  render(){
+    return(
+      <p>This is the list page.</p>
+    )
+  }
+}
+
+module.exports = List;
--- app/index.js
+++ app/index.js
@@ -2,9 +2,23 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 require('./index.css');
 var App = require('./components/App');
+var Detail = require('./components/Detail');
+var List = require('./components/List')
+var ReactRouter = require('react-router-dom');
+var Router = ReactRouter.BrowserRouter;
+var Route = ReactRouter.Route;
+var Switch = ReactRouter.Switch;
 
 
 ReactDOM.render(
-  <App />,
+  <Router>
+    <Switch>
+      <Route exact path="/" component={ List }/>
+      <Route exact path="/react" component={ Detail }/>
+      <Route render={function() {
+        return <p>Not Found</p>
+      }} />
+    </Switch>
+  </Router>,
   document.getElementById('app')
 )
